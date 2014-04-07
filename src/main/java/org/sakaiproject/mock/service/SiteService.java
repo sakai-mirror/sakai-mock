7a666b41c4d67b1c0dbcef44ee71da13c5a0b623
enietzel@anisakai.com <enietzel@anisakai.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-04-07 15:13:58 +0000

    KNL-1202 merge 307611 307613 307614 307615 307738 to 10.x
    git-svn-id: https://source.sakaiproject.org/svn/sakai-mock/branches/sakai-10.x@307866 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- src/main/java/org/sakaiproject/mock/service/SiteService.java
+++ src/main/java/org/sakaiproject/mock/service/SiteService.java
@@ -431,10 +431,6 @@ public class SiteService implements org.sakaiproject.site.api.SiteService {
     
     	public boolean getBooleanSiteProperty(String id, String propertyName){
 		return false;
-	}
-    
-	public boolean isJoinNotificationToggled(String id){
-		return false;
 	}	
     
     	public LinkedHashSet<String> getAllowedJoinableAccountTypeCategories(){
@@ -450,10 +446,6 @@ public class SiteService implements org.sakaiproject.site.api.SiteService {
 	}
     
     	public boolean isGlobalJoinGroupEnabled(){
-		return false;
-	}
-    
-    	public boolean isGlobalJoinNotificationEnabled(){
 		return false;
 	}
    
